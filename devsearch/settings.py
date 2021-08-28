af7ec0c705606d9a8bf1b3487fcd4805dcb09964
Isaac Rabiu <ziktech2020@gmail.com>
2021-08-28 14:47:58 +0100

    Updated the settings files

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG',default=True,cast=bool)
 
-ALLOWED_HOSTS = ['localhost','127.0.0.1','devsearch-env.eba-3ugpwq5r.us-west-2.elasticbeanstalk.com']
+ALLOWED_HOSTS = ['devsearch-env.eba-3ugpwq5r.us-west-2.elasticbeanstalk.com']
 
 
 # Application definition
